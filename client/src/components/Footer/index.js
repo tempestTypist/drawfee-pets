08b2cd2a42af20f2a3c2728ec588b8c88e705f03
Summer Villeneuve <svilleneuve1994@gmail.com>
2022-11-09 08:16:22 -0500

    fixed footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
   const location = useLocation();
   const history = useHistory();
+
   return (
     <div className="footer text-center">
-      <div>
-        {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            onClick={() => history.goBack()}
-          >
-            &larr; Go Back
-          </button>
-        )}
-        <h4>
-          <Link to="https://www.youtube.com/c/Drawfee">Drawfee Show on Youtube</Link>
-        </h4>
-      </div>
+      {location.pathname !== '/' && (
+        <button
+          className="btn btn-dark mb-3"
+          onClick={() => history.goBack()}
+        >
+          &larr; Go Back
+        </button>
+      )}
+      <p>Drawfee is a comedy drawing YouTube show where artists (primarily Nathan Yaffe, Jacob Andrews, Julia Lepetit-Andrews, and Karina Farek) turn audience suggestions into silly drawings.</p>
+      <p>Drawfee Pets is a lovingly made fan-project to give a home to these wayward drawings.</p>
+      <Link to="https://www.youtube.com/c/Drawfee">Drawfee Show on Youtube</Link>
+      <Link to="https://www.patreon.com/drawfee">Drawfee's Patreon</Link>
     </div>
   );
 };
